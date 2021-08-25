a0e802196643f29ebcb35203673b6b9915a12d65
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-08-25 21:54:27 +0300

    create config.py

---
+++ config.py
@@ -0,0 +1,32 @@
+import os
+
+class Config:
+    DEBUG = True
+    SECRET_KEY = 'mysterious_key'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oscar:123456789@localhost/myblog'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = 'test.user.python3.6@gmail.com'
+    MAIL_PASSWORD = 'codingstudent001'
+    
+    
+class ProdConfig(Config):
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+    
+class TestConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oscar:123456789@localhost/pitchapp'
+    
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://oscar:123456789@localhost/pitchapp'
+    
+    DEBUG = True
+    ENV = 'development'
+    
+config_options = {
+    'development':DevConfig,
+    'production':ProdConfig,
+    'test':TestConfig
+}
