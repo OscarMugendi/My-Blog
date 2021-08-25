f440e019223787fd2581352bd0433bd03f533e3b
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-08-25 21:51:38 +0300

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,7 @@
+export MAIL_USERNAME='test.user.python3.6@gmail.com'
+export MAIL_PASSWORD='codingstudent001'
+export SECRET_KEY='mysterious_key'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://oscar:123456789@localhost/myblog'
+
+
+python3 manage.py server
