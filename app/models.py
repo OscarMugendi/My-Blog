c923f0706cb212cfbf0cce0c12191d00c92a613f
OscarMugendi <87582596+OscarMugendi@users.noreply.github.com>
2021-08-26 12:05:40 +0300

    New model class structure

--- app/models.py
+++ app/models.py
@@ -5,6 +5,9 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin, current_user
 
 
+@login_manager.user_loader
+def loader_user(user_id):
+    return User.query.get(int(user_id))
 
 class User(db.Model, UserMixin):
     __tablename__="users"
@@ -13,6 +16,8 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(255), unique=True, nullable=False, index=True)
     email = db.Column(db.String(255), unique=True, nullable=False, index=True)
     encryptedpassword = db.Column(db.String(255),index=True)
+    posts = db.relationship('Post', backref='user', lazy='dynamic')
+    comments = db.relationship('Comment', backref='user', lazy='dynamic') 
 
     @property
     def password(self):
@@ -34,10 +39,6 @@ class User(db.Model, UserMixin):
         return f"User('{self.username}', '{self.email}'"
 
 
-@login_manager.user_loader
-def load_user(user_id):
-    return User.query.get(int(user_id))
-
 
 class Post(db.Model):
     __tablename__="posts"
@@ -46,6 +47,56 @@ class Post(db.Model):
     title = db.Column(db.String(100), nullable=False)
     date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
     content = db.Column(db.Text, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+
+    @classmethod
+    def get_post(cls,id):
+        post = Post.query.filter_by(id=id).first()
+
+        return post
+
+    @classmethod
+    def count_posts(cls,uname):
+        user = User.query.filter_by(username=uname).first()
+        posts = Post.query.filter_by(user_id=user.id).all()
+
+        posts_count = 0
+        for post in posts:
+            posts_count += 1
+
+        return posts_count
+
 
     def __repr__(self):
         return f"Post('{self.title}', '{self.date_posted}')"
+
+
+class Comment(db.Model):
+    __tablename__ = 'comments'
+
+    id = db.Column(db.Integer,primary_key = True)
+    comment = db.Column(db.String(1000))
+    user_id = db.Column(db.Integer,db.ForeignKey("users.id"))
+    post = db.Column(db.Integer,db.ForeignKey("posts.id"))
+
+    def save_comment(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    @classmethod
+    def get_comments(cls,post):
+        comments = Comment.query.filter_by(post_id=post).all()
+        return comments
